fae242023c1de42ed56833c208a4de58ed74edad
Ivan Vetrov <i.vetrov@gmail.com>
2015-10-29 01:20:22 +0300

    logic update - just a bit

--- parser.js
+++ parser.js
@@ -3,9 +3,9 @@ var parser = function(string, map) {
     for(var key in map[item]) {
       string = string.replace(new RegExp('\\{{2}\\s' + item + '\\.'+ key +'\\s\\}{2}', 'g'), map[item][key]);
     }
-    string = string.replace(new RegExp('\\{{2}\\s' + item + '\\.[a-zA-Z1-9-_]+\\s\\}{2}', 'g'), '<nothing>');
+    string = string.replace(new RegExp('\\{{2}\\s' + item + '\\.[a-zA-Z0-9-_]+\\s\\}{2}', 'g'), '<nothing>');
   }
-  return string;
+  return string.replace(/\{{2}\s[0-9A-Fa-f]{8}\.[a-zA-Z0-9-_]+\s\}{2}/g, '<nothing>');;
 };
 
 module.exports = parser;
